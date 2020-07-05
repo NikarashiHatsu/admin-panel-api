2ee733242dfe6b4d54f6ea932d70a2c57b770b45
NikarashiHatsu <yourlovelydev@gmail.com>
2020-07-05 20:07:55 +0700

    Improvisasi komentar

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Rute autentikasi
 Route::post('/login', 'Api\UserController@login')->name('api.login');
 Route::post('/register', 'Api\UserController@register')->name('api.register');
 
 Route::group(['middleware' => 'auth:api'], function() {
+    // Rute logout
     Route::get('/logout', 'Api\UserController@logout')->name('api.logout');
+
+    // Rute user management
+    Route::resource('user', 'Api\UserManagement')->except(['create', 'edit']);
 });
