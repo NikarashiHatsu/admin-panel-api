14af9c082590699b4ebdba0ed978ef339aec488f
NikarashiHatsu <yourlovelydev@gmail.com>
2020-07-05 21:13:20 +0700

    Added detailed information about patients

--- database/migrations/2020_07_05_132457_create_patients_table.php
+++ database/migrations/2020_07_05_132457_create_patients_table.php
@@ -15,7 +15,14 @@ class CreatePatientsTable extends Migration
     {
         Schema::create('patients', function (Blueprint $table) {
             $table->id();
-            $table->string('nama_lengkap', 255);
+            $table->string('nama_lengkap', 64);
+            $table->char('nik', 16);
+            $table->text('alamat');
+            $table->string('no_rekam_medis', 64);
+            $table->char('tinggi_badan', 3);
+            $table->char('berat_badan', 3);
+            $table->enum('peranan_keluarga', ['ayah', 'ibu', 'anak']);
+            $table->text('riwayat_penyakit')->nullable();
             $table->timestamps();
         });
     }
