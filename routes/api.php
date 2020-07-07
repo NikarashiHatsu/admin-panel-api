757f45154a4996bb9211a369012c802c9e580f51
NikarashiHatsu <yourlovelydev@gmail.com>
2020-07-07 10:43:01 +0700

    Mendaftarkan rute AppManagement ke api.php

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Rute autentikasi
-Route::post('/login', 'Api\UserController@login')->name('api.login');
-Route::post('/register', 'Api\UserController@register')->name('api.register');
+Route::post('login', 'Api\UserController@login')->name('api.login');
+Route::post('register', 'Api\UserController@register')->name('api.register');
 
 Route::group(['middleware' => 'auth:api'], function() {
     // Rute logout
-    Route::get('/logout', 'Api\UserController@logout')->name('api.logout');
+    Route::get('logout', 'Api\UserController@logout')->name('api.logout');
 
     // Rute user management
     Route::resource('user', 'Api\UserManagement')->except(['create', 'edit']);
     Route::resource('patient', 'Api\PatientController')->except(['create', 'edit']);
     Route::resource('article', 'Api\ArticleController')->except(['create', 'edit']);
+
+    // App management - base url
+    Route::get('get_base_url', 'Api\AppManagementController@get_base_url')->name('api.app.base_url');
+    Route::post('store_base_url', 'Api\AppManagementController@store_base_url')->name('api.app.store_base_url');
+    Route::put('update_base_url', 'Api\AppManagementController@update_base_url')->name('api.app.update_base_url');
 });
